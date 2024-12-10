896d5267c8cb62553fde1336038a1425f229fcfe
sumit timori <timori52@gmail.com>
2024-12-10 16:48:11 +0530

    fcm token

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,24 @@ import { auth } from "./firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import Nav from "./components/Nav";
 import "./App.css";
-import {FcmTokenProvider} from "./components/FcmTokenContext"
+import { FcmTokenProvider } from "./components/FcmTokenContext";
+// import { useFCM } from "./components/FcmTokenContext";
 
 function App() {
   const [user] = useAuthState(auth);
-  
+
   return (
     <>
-
-    <FcmTokenProvider>
-
-
-      <div className=" flex  sm:w-[600px] md:w-[728px]    mx-auto  text-center justify-center mt-10 ">
-        <section className="flex w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl   mx-2 sm:mx-4 flex-col rounded-2xl shadow-blue-400 shadow-2xl  bg-gray-100    h-[80vh]">
-          {/* Navbar */}
-          <Nav />
-          {/* Chat */}
-          {user ? <Chat /> : null}
-        </section>
-      </div>
-    </FcmTokenProvider>
+      <FcmTokenProvider>
+        <div className=" flex  sm:w-[600px] md:w-[728px]    mx-auto  text-center justify-center mt-10 ">
+          <section className="flex w-full max-w-sm sm:max-w-md md:max-w-lg lg:max-w-xl   mx-2 sm:mx-4 flex-col rounded-2xl shadow-blue-400 shadow-2xl  bg-gray-100    h-[80vh]">
+            {/* Navbar */}
+            <Nav />
+            {/* Chat */}
+            {user ? <Chat /> : null}
+          </section>
+        </div>
+      </FcmTokenProvider>
     </>
   );
 }
--- src/components/FcmTokenContext.jsx
+++ src/components/FcmTokenContext.jsx
@@ -21,7 +21,6 @@ export  const  FcmTokenProvider = ({children}) => {
           console.log("HERE'S THE TOKEN", fcmToken);
         }
       };
-      notificationPermission();
    
 
 
